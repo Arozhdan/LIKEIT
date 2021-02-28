2d29cdfef90c8f0bf61448bd45a0216fff9ccd6e
Arozhdan Baibussynov <halala30@gmail.com>
2021-02-28 12:45:01 +0100

    homepage markup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
+    <!-- <VueCustomScrollbar class="scroll-area" :settings="settings"> -->
     <Navbar />
     <Nuxt />
+    <!-- </VueCustomScrollbar> -->
   </div>
 </template>
 <script>
+import 'vue-custom-scrollbar/dist/vueScrollbar.css'
 import Navbar from '../components/Navbar.vue'
 export default {
-  components: { Navbar }
+  components: { Navbar },
+  data () {
+    return {
+      settings: {
+        suppressScrollX: false,
+        wheelSpeed: 0.6,
+        swipeEasing: true,
+        wheelPropagation: true
+
+      }
+    }
+  }
 }
 </script>
+
+<style lang="postcss" scoped>
+.scroll-area {
+  position: relative;
+  margin: auto;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vue-gallery.js', ssr: false },
+    { src: '~/plugins/vue-scroll.js', ssr: false },
     { src: '~/plugins/slider.js', ssr: false }
   ],
 
